604d19c2ed497e4ab6a975fd94579dcb6a4638f8
AVILEZ <AVILEZ@DESKTOP-0MT6DM4>
2020-06-17 13:57:20 +0000

    Verificando la funcionalidad de gitlap

---
+++ mProyectoCM/src/mproyectocm/MProyectoCM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mproyectocm;
+
+/**
+ *
+ * @author AVILEZ
+ */
+public class MProyectoCM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("hello word");
+       System.out.println("Juan Avilez");
+    }
+    
+}
